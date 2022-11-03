b65ed570bb0437d84844230cd161ac5897549394
gabdong <hwan970104@gmail.com>
2022-11-03 18:58:27 +0900

    ADD : routes folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Nav from "./components/Nav";
 import Header from "./components/Header";
+import Home from "./routes/Home";
 
 function App() {
   return (
     <div>
       <Header />
       <Nav />
+      <Router basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
