13789e1aab9d152167bf3f8c64cda36ffdc0b846
gabdong <hwan970104@gmail.com>
2022-11-04 19:16:13 +0900

    add font

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Home from "./routes/Home";
