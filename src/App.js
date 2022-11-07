3e4fd7c80e4f130596b7e50f7b755926f4e74be6
gabdong <hwan970104@gmail.com>
2022-11-08 01:17:44 +0900

    파일구조정리중

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header";
-import Home from "./routes/Home";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Header />
-      <Nav />
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-        </Routes>
-      </Router>
+      <div>
+        <Nav />
+        <Router basename={process.env.PUBLIC_URL}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
