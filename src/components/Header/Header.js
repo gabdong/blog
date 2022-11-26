34bbadec318a9331b96a93f0c4d905eefba23755
gabdong <hwan970104@gmail.com>
2022-11-27 00:04:48 +0900

    header styled-components 적용

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,32 @@
 import { FaSearch as Search } from "react-icons/fa";
-import { styled } from "styled-components";
+import styled from "styled-components";
 
-// const NavBtnStyle = styled``;
+const HeaderSt = styled.header`
+  width: 100%;
+  padding: 20px 0;
+  position: sticky;
+  left: 0;
+  top: 0;
+`;
+const LoginBtn = styled.button`
+  font-size: 18px;
+`;
+const Logo = styled.h1`
+  font-family: "SUIT-Bold";
+  font-size: 24px;
+`;
 
 function Header() {
   return (
-    <header id="header" className="header disFlex">
-      <h1 className="logo">Gabdong</h1>
-      <div className="disFlex">
+    <HeaderSt id="header" className="header disFlex spaceBetween">
+      <Logo>Gabdong</Logo>
+      <div className="disFlex alignItemsCenter gap10">
         <button>
           <Search />
         </button>
-        <button>Log In</button>
+        <LoginBtn>Log In</LoginBtn>
       </div>
-    </header>
+    </HeaderSt>
   );
 }
 
