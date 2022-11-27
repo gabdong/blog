74879c4b5cfcbb055f3abed4df87689209614db0
gabdong <hwan970104@gmail.com>
2022-11-28 02:16:33 +0900

    no message

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,12 +2,16 @@ import styled from "styled-components";
 import NavBtn from "./NavBtn";
 
 const NavSt = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
   width: 200px;
 `;
 function Nav() {
   return (
     <NavSt id="nav">
       <NavBtn path="/" text="home" />
+      <NavBtn path="/board" text="Board" />
     </NavSt>
   );
 }
