d032d51aa6e1cb13648b7c3a51872cdd32d3efe2
gabdong <hwan970104@gmail.com>
2022-11-28 19:26:43 +0900

    styled-components 적용중

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import styled from 'styled-components';
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
 import Pages from "./pages/Pages";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.main`
+  display: flex;
+  padding-top: 100px;
+`;
+
 function App() {
   return (
-    <div className="disFlex flexColumn" id="wrapper">
+    <Wrapper id="wrapper">
       <Header />
-      <div className="disFlex">
+      <Main>
         <Router basename={process.env.PUBLIC_URL}>
           <Nav />
           <Pages />
         </Router>
-      </div>
-    </div>
+      </Main>
+    </Wrapper>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,30 +2,38 @@ import { FaSearch as Search } from "react-icons/fa";
 import styled from "styled-components";
 
 const HeaderSt = styled.header`
+  display: flex;
+  justify-content: space-between;
   width: 100%;
   padding: 20px 0;
   position: sticky;
   left: 0;
   top: 0;
 `;
-const LoginBtn = styled.button`
-  font-size: 18px;
+const HeaderBtnWrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+const HeaderBtn = styled.button`
+  font-family: "SUIT-Medium";
+  font-size: 16px;
 `;
 const Logo = styled.h1`
   font-family: "SUIT-Bold";
-  font-size: 24px;
+  font-size: 22px;
 `;
 
 function Header() {
   return (
-    <HeaderSt id="header" className="header disFlex spaceBetween">
+    <HeaderSt id="header">
       <Logo>Gabdong</Logo>
-      <div className="disFlex alignItemsCenter gap10">
-        <button>
+      <HeaderBtnWrap>
+        <HeaderBtn>
           <Search />
-        </button>
-        <LoginBtn>Log In</LoginBtn>
-      </div>
+        </HeaderBtn>
+        <HeaderBtn>로그인</HeaderBtn>
+      </HeaderBtnWrap>
     </HeaderSt>
   );
 }
