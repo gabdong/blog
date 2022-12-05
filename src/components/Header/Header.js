75f67efc978f2b9437aa72292acd458a48318cdf
gabdong <hwan970104@gmail.com>
2022-12-05 10:51:28 +0900

    no message

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import { FaSearch as Search } from "react-icons/fa";
+import { useState } from "react";
 import styled from "styled-components";
+import LogIn from "../LogIn/LogIn";
 
 const HeaderSt = styled.header`
   display: flex;
@@ -22,18 +24,31 @@ const HeaderBtn = styled.button`
 const Logo = styled.h1`
   font-family: "SUIT-Bold";
   font-size: 1.3125rem;
+  color: var(--primary-color);
 `;
 
 function Header() {
+  const [logInView, setLogInView] = useState(false);
+  const logInWrapHandler = (e) => {
+    e.preventDefault();
+
+    setLogInView(!logInView);
+  };
+
   return (
     <HeaderSt id="header">
+      {/* //g Logo */}
       <Logo>Gabdong</Logo>
       <HeaderBtnWrap>
+        {/* //g Search Btn */}
         <HeaderBtn>
           <Search />
         </HeaderBtn>
-        <HeaderBtn>로그인</HeaderBtn>
+        {/* //g LogIn Btn */}
+        <HeaderBtn onClick={logInWrapHandler}>로그인</HeaderBtn>
       </HeaderBtnWrap>
+      {/* //g logInWrap */}
+      {logInView ? <LogIn handler={logInWrapHandler}></LogIn> : null}
     </HeaderSt>
   );
 }
