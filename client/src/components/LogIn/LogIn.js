6e279fe1d2a179076a00dec59ac34a172bb5560f
gabdong <hwan970104@gmail.com>
2022-12-09 02:43:13 +0900

    no message

--- client/src/components/LogIn/LogIn.js
+++ client/src/components/LogIn/LogIn.js
@@ -6,6 +6,7 @@ const LogInWrap = styled.article`
   justify-content: center;
   width: 100%;
   height: 100%;
+  overflow-y: auto;
   position: fixed;
   left: 0;
   top: 0;
@@ -15,23 +16,55 @@ const LogInOverlay = styled.div`
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
-  position: absolute;
+  position: fixed;
   left: 0;
   top: 0;
 `;
 const LogInContent = styled.div`
-  width: 500px;
+  width: 400px;
+  max-width: 90%;
   height: 300px;
+  padding: 20px;
   background: #fff;
   position: relative;
   z-index: 1;
+`;
+const LogInForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-item: center;
+  justify-content: center;
+  gap: 10px;
+  width: 100%;
+  height: 100%;
+`;
+//TODO input component 분리
+const LogInInput = styled.input`
+  all: unset;
+  width: 100%;
+  padding: 8px 12px;
+  border: 1px solid #ddd;
+  border-radius: var(--border-radius);
+  cursor: pointer;
+  transition: var(--transition);
+
+  &:active,
+  &:focus,
+  &:hover {
+    border: 1px solid var(--primary-color);
+  }
 `;
 
 function LogIn({ handler }) {
   return (
     <LogInWrap>
       <LogInOverlay onClick={handler} />
-      <LogInContent />
+      <LogInContent>
+        <LogInForm>
+          <LogInInput placeholder="Username" />
+          <LogInInput placeholder="Password" />
+        </LogInForm>
+      </LogInContent>
     </LogInWrap>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const api = require("./routes/index.js");
 const PORT = process.env.port || 9411;
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use("/api", api);
+
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
