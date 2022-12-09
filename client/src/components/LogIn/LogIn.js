158125053d276cf7d85c0373972159f899eb6314
gabdong <hwan970104@gmail.com>
2022-12-09 16:34:47 +0900

    no message

--- client/src/components/LogIn/LogIn.js
+++ client/src/components/LogIn/LogIn.js
@@ -15,7 +15,7 @@ const LogInWrap = styled.article`
 const LogInOverlay = styled.div`
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+  background: linear-gradient(180deg, var(--primary-color-d), transparent);
   position: fixed;
   left: 0;
   top: 0;
@@ -26,6 +26,7 @@ const LogInContent = styled.div`
   height: 300px;
   padding: 20px;
   background: #fff;
+  border-radius: var(--border-radius);
   position: relative;
   z-index: 1;
 `;
@@ -43,15 +44,17 @@ const LogInInput = styled.input`
   all: unset;
   width: 100%;
   padding: 8px 12px;
-  border: 1px solid #ddd;
+  border: 2px solid #ddd;
   border-radius: var(--border-radius);
+  font-family: "SUIT-Medium";
+  color: #000000;
   cursor: pointer;
   transition: var(--transition);
 
   &:active,
   &:focus,
   &:hover {
-    border: 1px solid var(--primary-color);
+    border: 2px solid var(--primary-color);
   }
 `;
 
@@ -62,7 +65,7 @@ function LogIn({ handler }) {
       <LogInContent>
         <LogInForm>
           <LogInInput placeholder="Username" />
-          <LogInInput placeholder="Password" />
+          <LogInInput type="password" placeholder="Password" />
         </LogInForm>
       </LogInContent>
     </LogInWrap>
