349e6f61a4961b9058ee46efe2a316a7811232c4
gabdong <hwan970104@gmail.com>
2022-12-12 03:01:17 +0900

    no message

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express();
+const db = require("../config/db");
 
-router.get('/test', (req,res) => {
-    res.send({ test : "this is test!!"});
+router.get("/test", (req, res) => {
+  db.query("SELECT * FROM member", (err, data) => {
+    res.send(data);
+  });
 });
 
 module.exports = router;
