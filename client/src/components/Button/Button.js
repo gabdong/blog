88fa397e50057817798465c7106bc66f396c860f
gabdong <hwan970104@gmail.com>
2022-12-11 17:11:17 +0900

    no message

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,14 +1,19 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const ButtonSt = styled.button`
-    padding: 8px 12px;
-    background-color: var(--primary-color-d);
-    color: var(--primary-color-d-text);
-    border-radius: var(--border-radius);
+  padding: 8px 12px;
+  background-color: #000000;
+  color: #ffffff;
+  border-radius: var(--border-radius);
+  transition: var(--transition);
+
+  &:hover {
+    background-color: var(--primary-color);
+  }
 `;
 
-function Button({text, classname}) {
-    return <ButtonSt className={`${classname} buttonText`}>{text}</ButtonSt>;
+function Button({ text, classname }) {
+  return <ButtonSt className={`${classname} buttonText`}>{text}</ButtonSt>;
 }
 
 export default Button;
