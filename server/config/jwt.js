005667d31a4d1d44fa669d464e2073b102d3a172
gabdong <hwan970104@gmail.com>
2022-12-21 21:14:46 +0900

    no message

--- server/config/jwt.js
+++ server/config/jwt.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const token = () => {
-    return {
-        access(id) {
-            return jwt.sign({id}, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: "15m",
-            });
-        },
-        refresh(id) {
-            return jwt.sign({id}, process.env.REFRESH_TOKEN_SECRET, {
-                expiresIn: '1days',
-            });
-        },
-        check(token, mode) {
-            const secret_key = mode == 'access' ? process.env.ACCESS_TOKEN_SECRET : process.env.REFRESH_TOKEN_SECRET;
+  return {
+    access(id) {
+      return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+      });
+    },
+    refresh(id) {
+      return jwt.sign({ id }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "1days",
+      });
+    },
+    check(token, mode) {
+      const secret_key =
+        mode == "access"
+          ? process.env.ACCESS_TOKEN_SECRET
+          : process.env.REFRESH_TOKEN_SECRET;
 
-            return jwt.verify(token, secret_key);
-        }
-    }
-}
+      return jwt.verify(token, secret_key);
+    },
+  };
+};
 
 module.exports = token;
--- server/routes/user.js
+++ server/routes/user.js
@@ -17,18 +17,18 @@ router.post("/login", (req, res) => {
     `SELECT idx, name FROM member WHERE id='${id}' AND pw='${password}'`,
     (err, data) => {
       if (data.length === 0) {
-        res.status(500).json({error: "일치하는 회원 정보가 없습니다."});
+        res.status(500).json({ error: "일치하는 회원 정보가 없습니다." });
       } else if (data.length !== 0) {
         const user = data[0];
-        const {id} = user;
+        const { id } = user;
         const accessToken = token().access(id);
         const refreshToken = token().refresh(id);
 
-        res.cookie('auth', refreshToken, {
+        res.cookie("auth", refreshToken, {
           maxAge: 1000 * 60 * 60 * 24,
-          httpOnly: true
+          httpOnly: true,
         });
-        res.json({user, accessToken});
+        res.json({ user, accessToken });
       }
     }
   );
@@ -36,18 +36,18 @@ router.post("/login", (req, res) => {
 
 //g 권한조회
 router.get("/auth", (req, res) => {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers["authorization"];
   const accessToken = authHeader && authHeader.split(" ")[1];
 
   if (accessToken) {
-    const auth = token().check(accessToken, 'access');
+    const auth = token().check(accessToken, "access");
     console.log(auth);
   } else {
     const refreshToken = authHeader;
     console.log(cookieParser.JSONCookies(req.headers.cookie));
   }
 
-  res.send('hi');
+  res.send("hi");
 });
 
 module.exports = router;
