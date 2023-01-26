18fb553fb12ccda7d49d8ce34e6ed470051afe6e
gabdong <hwan970104@gmail.com>
2023-01-26 21:11:04 +0900

    게시판 목록 출력 작업 진행중

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,18 +1,33 @@
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 
+import { getBoardList } from "../../apis/boards.js";
+
 import NavBtn from "./NavBtn.js";
+import { useEffect, useState } from "react";
 
 function Nav() {
   const user = useSelector((store) => store.user);
   const { isLogin } = user;
+  const [boardList, setBoardList] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getBoardList(setBoardList, setLoading);
+  }, []);
+
+  const renderBoardList = () => {};
 
   return (
-    <NavSt id="nav">
-      <NavBtn path="/" text="Home" />
-      <NavBtn path="/board" text="Board" />
-      {isLogin ? <NavBtn path="/settings" text="Settings" /> : null}
-    </NavSt>
+    <>
+      {loading ? null : (
+        <NavSt id="nav">
+          <NavBtn path="/" text="Home" />
+          <NavBtn path="/board" text="Board" />
+          {isLogin ? <NavBtn path="/settings" text="Settings" /> : null}
+        </NavSt>
+      )}
+    </>
   );
 }
 
