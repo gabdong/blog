eb8ba9cab1e9c1ac6dc3811ce507755883da767a
gabdong <hwan970104@gmail.com>
2023-03-16 18:38:57 +0900

    REMOVE TEST CODE

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -14,7 +14,6 @@ import { getPost } from "../apis/posts";
 function Post() {
   const params = useParams();
   const user = useSelector((store) => store.user);
-  console.log(user);
   const { postIdx } = params;
   const [loading, setLoading] = useState(true);
   const [postData, setPostData] = useState({});
@@ -27,7 +26,6 @@ function Post() {
     memberName = postData[0].name;
     updateDatetime = postData[0].updateDatetime;
   }
-  console.log(postData);
 
   useEffect(() => {
     (async function () {
