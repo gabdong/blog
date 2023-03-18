34bfaf14f40f897cb95a81b0fc00b91de7030d6f
gabdong <hwan970104@gmail.com>
2023-03-19 04:53:20 +0900

    2023.03.19 페이지네이션 active스타일 추가

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import React from "react";
 
 /**
  * @param Number totalCnt: 컨텐츠 총갯수
@@ -10,6 +10,8 @@ import { NavLink } from "react-router-dom";
  * @param Number limit: 컨텐츠 갯수
  */
 function Pagination({ totalCnt, page, paginationCnt = 10, path, limit = 10 }) {
+  const location = useLocation();
+
   if (totalCnt <= limit) return null; // 컨텐츠 총갯수가 limit보다 작을경우 출력 x
 
   let startNum = Math.floor(page / paginationCnt) * paginationCnt + 1;
@@ -31,7 +33,14 @@ function Pagination({ totalCnt, page, paginationCnt = 10, path, limit = 10 }) {
         return (
           <NavLink
             key={pageNum}
-            className="smallTitle"
+            className={() => {
+              const currentPage = Number(
+                new URLSearchParams(location.search).get("page")
+              );
+              let className = "smallTitle";
+              if (currentPage === pageNum) className += " active";
+              return className;
+            }}
             to={`${path}?page=${pageNum}`}
           >
             {pageNum}
@@ -57,10 +66,14 @@ const PaginationSt = styled.div`
     width: 20px;
     height: 20px;
     padding: 4px;
-    background: red;
     border-radius: var(--border-radius);
+    background: var(--gray);
     cursor: pointer;
+  }
+
+  & > a.active {
+    background: var(--primary-color);
   }
 `;
 
-export default Pagination;
+export default React.memo(Pagination);
