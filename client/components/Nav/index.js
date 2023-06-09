c38d252bfa1bad5b178efaa863422443a1e9fa75
gabdong <hwan970104@gmail.com>
2023-06-09 19:25:11 +0900

    Feat: 네비게이션 작업 시작

---
+++ client/components/Nav/index.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export default function Nav() {
+    return <div></div>;
+}
+
+const NavSt = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 140px;
+  min-width: 140px;
+
+  @media all and (max-width: 767px) {
+    gap: 12px;
+
+    width: 80%;
+    height: 100%;
+    padding: 20px;
+    background: rgba(0, 0, 0, 0.8);
+    overflow-y: auto;
+    position: fixed;
+    left: -80%;
+    top: 0;
+    z-index: 32;
+    transition: var(--transition);
+
+    &::-webkit-scrollbar {
+      width: 3px;
+      background: var(--dark-l);
+      border-radius: var(--border-radius);
+    }
+    &::-webkit-scrollbar-thumb {
+      background: var(--primary-color);
+      border-radius: var(--border-radius);
+    }
+
+    &.active {
+      left: 0;
+    }
+  }
+`;
+const NavBackgroundSt = styled.div`
+  @media all and (max-width: 767px) {
+    &.active {
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.8);
+      position: fixed;
+      left: 0;
+      top: 0;
+      z-index: 31;
+    }
+  }
+`;
+const CloseBtnSt = styled(Close)`
+  font-size: 32px;
+  cursor: pointer;
+`;
