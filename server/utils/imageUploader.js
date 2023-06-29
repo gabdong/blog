b3d13cc1690e3df81912d1977c8b26feefbcc07e
gabdong <hwan970104@gmail.com>
2023-06-29 18:56:01 +0900

    Docs: aws-sdk 버전 변경

--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -1,9 +1,11 @@
-const AWS = require("aws-sdk");
+const {
+  S3
+} = require("@aws-sdk/client-s3");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const path = require("path");
 
-const s3 = new AWS.S3({
+const s3 = new S3({
   region: process.env.AWS_S3_REGION,
   accessKeyId: process.env.AWS_S3_ACCESS_KEY,
   secretAccessKey: process.env.AWS_S3_SECRET_KEY,
