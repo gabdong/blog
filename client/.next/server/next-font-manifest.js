8c454c319c8acb84c0c95a477c57bbe15132906d
gabdong <hwan970104@gmail.com>
2024-04-29 01:01:15 +0900

    no message

--- client/.next/server/next-font-manifest.js
+++ client/.next/server/next-font-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
-=======
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
->>>>>>> f9e9a87 (no message)
